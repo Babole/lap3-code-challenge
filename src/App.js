7241ff2dee9e792aba10d093f73e59f60fa990b7
Stefan Mircea <stefancmircea@gmail.com>
2022-10-05 09:21:23 +0100

    add react setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route } from 'react-router-dom'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
